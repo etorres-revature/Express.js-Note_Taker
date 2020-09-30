d6ca2ee8dc558e4eb6791a1ab98b7d26a99308b7
Eric D. Torres <etorresnotary@gmail.com>
2020-09-30 11:43:51 -0500

    adding get for notes page

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var PORT = process.env.PORT || 25789;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", function (req, res) {
+app.get("", function (req, res) {
     res.sendFile(path.join(__dirname + "/public", "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public", "notes.html"));
 });
 
 app.listen(PORT, function () {
