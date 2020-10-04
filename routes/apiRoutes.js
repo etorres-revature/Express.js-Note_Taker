404f529496c6f13b0d02e586535cb29dcc556210
Eric D. Torres <etorresnotary@gmail.com>
2020-10-04 12:12:48 -0500

    updating package.json and commmenting both server code and route file code (html/api)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,55 +1,87 @@
-var fs = require("fs");
+//require the file system built-in library for NODE.js
+const fs = require("fs");
 
 module.exports = function (app) {
+  // * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   app.get("/api/notes", function (req, res) {
+    //read the db.json file to get all notes
     fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+      //error handling
       if (err) throw err;
+      //parse the JSON in db.json
       let notes = JSON.parse(data);
+      //return the parsed information in the notes variable to display on screen to user
       return res.json(notes);
     });
   });
 
+  //* POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
   app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
+    //create variable to hold the body with information entered by the user
+    const newNote = req.body;
+    //empty array to hold the notes in the db.json file
     let allNotes = [];
+    //variable for the id
     let id = 0;
-    fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, data) {
+    //read the db.json file
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
+      //error handling
       if (err) throw err;
+      //parsing the JSON data and setting it as the value for the empty array
       allNotes = JSON.parse(data);
+      //for loop to go through all of the objects in the db.json array
       for (let i = 0; i < allNotes.length; i++) {
+        //if logic to compare the current value of the id variable with the values of the ids in the array of object in db.json
         if (allNotes[i].id > id) {
+          //if the objects id is greater than the current value of id setting the id variable to the id of the object
+          //this logic will find the highest id value in the array of objects and set it to the id variable
           id = allNotes[i].id;
         }
       }
+      //setting the id for the new note to the highest value of the id in the array of objects in db.json and adding one
       newNote.id = parseInt(id) + 1;
+      //pushing the new note information onto the allNotes array
       allNotes.push(newNote);
-
+      //write the db.json file with the new array of notes including the user entered informaiton in the body of the request
       fs.writeFile(
         __dirname + "/../db/db.json",
+        //stringify the JSON array
         JSON.stringify(allNotes),
         "utf8",
         function (err) {
+          //error handling
           if (err) throw err;
+          //telling the program that the function is over
           res.end();
         }
       );
     });
   });
 
+  //* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
   app.delete("/api/notes/:id", function (req, res) {
+    //taking in the id from the note selected to be deleted
     const deleteID = req.params.id;
-    fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, data) {
+    //read the db.json fil
+    fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
+      //error handling
       if (err) throw err;
+      //creating a new array using the filter over the parsed information from db.json
       const deleteNote = JSON.parse(data).filter((entry) => {
+        //if the "entry" does not have the same id as the one deleted it is put back into the array
         return entry.id != deleteID;
       });
+      //write the db.json file with the new array excluding the user deleted information
       fs.writeFile(
         __dirname + "/../db/db.json",
+        //stringify the JSON array
         JSON.stringify(deleteNote),
         "utf8",
         function (err) {
+          //error handling
           if (err) throw err;
-          res.send();
+          //telling the program that the function is over
+          res.end();
         }
       );
     });
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+//require the express library
 const express = require("express");
 
+//create a variable for the express function and a dynamic variable for the server port
 const app = express();
 const PORT = process.env.PORT || 25789;
 
+//middleware for url encoding, json, and static display of HTML
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+//require the api routes and html routes files
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+//turning on the server
 app.listen(PORT, function (req, res) {
   console.log("App listening on PORT: " + PORT);
 });
